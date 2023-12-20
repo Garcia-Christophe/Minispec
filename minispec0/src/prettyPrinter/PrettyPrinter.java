f0f965d25aaed626795871b1bee5bdfc752bb694
louis <louisdosnon@gmail.com>
2023-12-20 11:12:09 +0100

    patch PrettyPrinter

--- minispec0/src/prettyPrinter/PrettyPrinter.java
+++ minispec0/src/prettyPrinter/PrettyPrinter.java
@@ -26,7 +26,7 @@ public class PrettyPrinter extends Visitor {
 
 	@Override
 	public void visitModel(Model e) {
-		packageDir = new File("minispec0/src/spec");
+		packageDir = new File("src/spec");
 		if (!packageDir.exists())
 			packageDir.mkdir();
 	}
